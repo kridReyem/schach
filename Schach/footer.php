a827523bd587e4e74df0563b716d4baca256521f
maggonski <31771839+maggonski@users.noreply.github.com>
2017-09-12 19:37:22 +0200

    v2.1 footer variante 2

--- Schach/footer.php
+++ Schach/footer.php
@@ -4,64 +4,73 @@
     
 	
 	</div>
-    <!----------- Footer ------------>
-    <section id="footer" style="background-color:#292b34;">
-        <div class="container">
-    		<center style="color:#fff;">
-    		<div class="container">
-    		    <div class="well well-sm main-footer"  style="background-color:#292b34; border:0px">
-    		      
-    		      
-    		                <div class="row">
-    		                    <div class="col-md-4">
-    		                        <p>Der Verein ist im Vereinsregister eingetragen unter:</p>
-                                    <p>Amtsgericht Offenbach am Main</p>
-                                    <p> 5 VR 1026</p>
-                                    <p>Amtsgericht Offenbach am Main</p>
-                                
-    		                    </div><!--end .col-md-4-->
-                                
-    		                   <div class="col-md-4">
-    		                        <div class="row contact row-first">
-    		                            <div class="col-md-12">
-    		                                  <p><a href="mailto: 1.vorsitzender@vsg-1880-offenbach.de">mail@vsgo.de</a></p>
-    		                            </div><!--end .col-md-6-->
-    		                           
-    		                        </div><!--end .row .contact .row-first-->
-    		                        <div class="row contact row-second">
-    		                            <div class="col-md-12">
-    		                                 <h3>© Copyright M. Rich & K. Reyem</h3>
-    		                            </div><!--end .col-md-12-->
-    		                          
-    		                        </div><!--end .row .contact-->
-    		                    </div><!--end .col-md-6-->
-                                
-                                
-    		                    <div class="col-md-4">
-    		                        <div class="row contact row-first">
-    		                            <div class="col-md-6">
-    		                                <a href="https://www.facebook.com/" class="skype"><i class="fa fa-skype"></i>Facebook</a>
-    		                            </div><!--end .col-md-6-->
-    		                            <div class="col-md-6">
-    		                                <a href="http://www.vsg-1880-offenbach.de/" class="instagram"><i class="fa fa-instagram"></i>Unsere Alte Seite</a>
-    		                            </div><!--end .col-md-6-->
-    		                        </div><!--end .row .contact .row-first-->
-    		                        <div class="row contact row-second">
-    		                            <div class="col-md-6">
-    		                                <a href="https://www.google.de/maps/dir//Arthur-Zitscher-Stra%C3%9Fe+22,+63065+Offenbach+am+Main/@50.1068585,8.7696899,16z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x47bd11fda228386f:0x4df033e30aee111e!2m2!1d8.7740673!2d50.1068586?hl=de" class="facebook"><i class="fa fa-facebook"></i>Anfahrt</a>
-    		                            </div><!--end .col-md-6-->
-    		                            <div class="col-md-6">
-    		                                <a href="https://www.google.de/" class="twitter"><i class="fa fa-twitter"></i>Noch Fragen?</a>
-    		                            </div><!--end .col-md-6-->
-    		                        </div><!--end .row .contact-->
-    		                    </div><!--end .col-md-6-->
-    		                </div><!--end .row-->
-    		            </div><!--end .col-md-12-->
-    		        </div><!--end .row-->
-    		   
-    		
-    	
-    </section><!--end #footer-->
+    <footer>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4 col-sm-6 footerleft ">
+        <div class="logofooter"> VSG 1880 OFFENBACH</div>
+        <p>Diese Seite wird betrieben von der:
+
+Vereinigten Schachgesellschaft 1880 Offenbach am Main e.V.
+
+Der Verein ist im Vereinsregister eingetragen unter:
+
+Amtsgericht Offenbach am Main
+5 VR 1026
+
+Vertretungsberechtigter des Vereins im Außenverhältnis ist derzeit:
+
+Ulf Neumann
+Paulstraße 25
+63150 Heusenstamm
+Tel.: 06104/9497151</p>
+        
+        
+      </div>
+      <div class="col-md-2 col-sm-6 paddingtop-bottom">
+        <h6 class="heading7">Links</h6>
+        <ul class="footer-ul">
+          <li><a href="mailto:mail@vsgo.com"> Kontakt</a></li>
+          <li><a href="#"> Impressum</a></li>
+       
+          
+        </ul>
+      </div>
+      <div class="col-md-3 col-sm-6 paddingtop-bottom">
+        <h6 class="heading7">Anfahrt</h6>
+        <div class="post">
+          <p>HIER KOMMT EINE MAP HIN</p>
+         
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6 paddingtop-bottom">
+        <div class="fb-page" data-href="https://www.facebook.com/facebook" data-tabs="timeline" data-height="300" data-small-header="false" style="margin-bottom:15px;" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
+          <div class="fb-xfbml-parse-ignore">
+            <blockquote cite="https://www.facebook.com/facebook"><a href="https://www.facebook.com/facebook">Facebook</a></blockquote>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
+<!--footer start from here-->
+
+<div class="copyright">
+  <div class="container">
+    <div class="col-md-6">
+      <p>© 2017 - All Rights by M. Rich & K. Reyem</p>
+    </div>
+    <div class="col-md-6">
+      <ul class="bottom_ul">
+ 
+        <li><a href="#">Blog</a></li>
+        <li><a href="#">Faq's</a></li>
+        <li><a href="#">Contact us</a></li>
+        <li><a href="#">Site Map</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
     <!-- Sticky Footer -->
     <script type="text/javascript" src="js/footer.js"></script>
     <!-- Fade Contact Footer -->
--- Schach/index.php
+++ Schach/index.php
@@ -32,6 +32,163 @@
             <p> Reihe 2 Artikel 2</p>
             </div>
         </div>
+    <div class="row">
+            <div class="col-md-6">
+            <p>Reihe 2 Artikel 1</p>
+            
+            </div>
+        <div class="col-md-6">
+            <p> Reihe 2 Artikel 2</p>
+            </div>
+        </div>
+    <div class="row">
+            <div class="col-md-6">
+            <p>Reihe 2 Artikel 1</p>
+            
+            </div>
+        <div class="col-md-6">
+            <p> Reihe 2 Artikel 2</p>
+            </div>
+        </div>
+    <div class="row">
+            <div class="col-md-6">
+            <p>Reihe 2 Artikel 1</p>
+            
+            </div>
+        <div class="col-md-6">
+            <p> Reihe 2 Artikel 2</p>
+            </div>
+        </div><div class="row">
+            <div class="col-md-6">
+            <p>Reihe 2 Artikel 1</p>
+            
+            </div>
+        <div class="col-md-6">
+            <p> Reihe 2 Artikel 2</p>
+            </div>
+        </div><div class="row">
+            <div class="col-md-6">
+            <p>Reihe 2 Artikel 1</p>
+            
+            </div>
+        <div class="col-md-6">
+            <p> Reihe 2 Artikel 2</p>
+            </div>
+        </div><div class="row">
+            <div class="col-md-6">
+            <p>Reihe 2 Artikel 1</p>
+            
+            </div>
+        <div class="col-md-6">
+            <p> Reihe 2 Artikel 2</p>
+            </div>
+        </div>
+    <div class="row">
+            <div class="col-md-6">
+            <p>Reihe 2 Artikel 1</p>
+            
+            </div>
+        <div class="col-md-6">
+            <p> Reihe 2 Artikel 2</p>
+            </div>
+        </div><div class="row">
+            <div class="col-md-6">
+            <p>Reihe 2 Artikel 1</p>
+            
+            </div>
+        <div class="col-md-6">
+            <p> Reihe 2 Artikel 2</p>
+            </div>
+        </div><div class="row">
+            <div class="col-md-6">
+            <p>Reihe 2 Artikel 1</p>
+            
+            </div>
+        <div class="col-md-6">
+            <p> Reihe 2 Artikel 2</p>
+            </div>
+        </div><div class="row">
+            <div class="col-md-6">
+            <p>Reihe 2 Artikel 1</p>
+            
+            </div>
+        <div class="col-md-6">
+            <p> Reihe 2 Artikel 2</p>
+            </div>
+        </div><div class="row">
+            <div class="col-md-6">
+            <p>Reihe 2 Artikel 1</p>
+            
+            </div>
+        <div class="col-md-6">
+            <p> Reihe 2 Artikel 2</p>
+            </div>
+        </div><div class="row">
+            <div class="col-md-6">
+            <p>Reihe 2 Artikel 1</p>
+            
+            </div>
+        <div class="col-md-6">
+            <p> Reihe 2 Artikel 2</p>
+            </div>
+        </div><div class="row">
+            <div class="col-md-6">
+            <p>Reihe 2 Artikel 1</p>
+            
+            </div>
+        <div class="col-md-6">
+            <p> Reihe 2 Artikel 2</p>
+            </div>
+        </div><div class="row">
+            <div class="col-md-6">
+            <p>Reihe 2 Artikel 1</p>
+            
+            </div>
+        <div class="col-md-6">
+            <p> Reihe 2 Artikel 2</p>
+            </div>
+        </div><div class="row">
+            <div class="col-md-6">
+            <p>Reihe 2 Artikel 1</p>
+            
+            </div>
+        <div class="col-md-6">
+            <p> Reihe 2 Artikel 2</p>
+            </div>
+        </div>
+    <div class="row">
+            <div class="col-md-6">
+            <p>Reihe 2 Artikel 1</p>
+            
+            </div>
+        <div class="col-md-6">
+            <p> Reihe 2 Artikel 2</p>
+            </div>
+        </div><div class="row">
+            <div class="col-md-6">
+            <p>Reihe 2 Artikel 1</p>
+            
+            </div>
+        <div class="col-md-6">
+            <p> Reihe 2 Artikel 2</p>
+            </div>
+        </div><div class="row">
+            <div class="col-md-6">
+            <p>Reihe 2 Artikel 1</p>
+            
+            </div>
+        <div class="col-md-6">
+            <p> Reihe 2 Artikel 2</p>
+            </div>
+        </div><div class="row">
+            <div class="col-md-6">
+            <p>Reihe 2 Artikel 1</p>
+            
+            </div>
+        <div class="col-md-6">
+            <p> Reihe 2 Artikel 2</p>
+            </div>
+        </div>
 </div>
 <div class="alert alert-info alert-dismissable fade in">
     <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
